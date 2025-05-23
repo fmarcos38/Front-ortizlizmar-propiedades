34f00eed02858c31af21f4dce79d1a6987e9fe94
fmarcos38 <fmarcos_23@hotmail.com>
2025-05-23 19:07:27 -0300

    lista props destacadas

--- src/Components/ListaPropiedades/index.jsx
+++ src/Components/ListaPropiedades/index.jsx
@@ -3,34 +3,62 @@ import Card from '../Card';
 import NoHayProps from '../NoHayProps';
 import './styles.css';
 
-function ListaPropiedades({allProps}) {
+function ListaPropiedades({ allProps }) {
+
+    /* lógica para mostrar primero las destacadas */
+    const propsDestacadas = allProps.filter(p => p.destacadaEnWeb === true);
+    const propsNoDestacadas = allProps.filter(p => p.destacadaEnWeb === false);
 
     return (
-        <div className='contGralListaP'>       
+        <div className='contGralListaP'>
             <div className='contListaP'>
                 {
-                    allProps[0] ?
-                    allProps.map(p => {
-                        return (
-                            <div className='cont-card' key={p.id}>
-                                <Card className='card' key={p.id}
-                                    id={p.id}                                    
-                                    direccionF={p.direccionF}
-                                    operacion={p.operacion}
-                                    imagenes={p.imagenes}
-                                    tituloPublicacion={p.tituloPublicacion}
-                                    ambientes={p.ambientes}
-                                    dormitorios={p.dormitorios}
-                                    unidadMedida={p.unidadMedida}
-                                    cantCocheras={p.cantCocheras}
-                                    supTotal={p.supTotal}
-                                    tipo={p.tipo}                           
-                                />
-                            </div>
-                        )
-                    }) : (
+                    (propsDestacadas?.length > 0 || propsNoDestacadas?.length > 0) ? (
+                        <>
+                            {
+                                propsDestacadas?.map(p => (
+                                    <div className='cont-card' key={p.id}>
+                                        <Card
+                                            className='card'
+                                            id={p.id}
+                                            direccionF={p.direccionF}
+                                            operacion={p.operacion}
+                                            imagenes={p.imagenes}
+                                            tituloPublicacion={p.tituloPublicacion}
+                                            ambientes={p.ambientes}
+                                            dormitorios={p.dormitorios}
+                                            unidadMedida={p.unidadMedida}
+                                            cantCocheras={p.cantCocheras}
+                                            supTotal={p.supTotal}
+                                            tipo={p.tipo}
+                                        />
+                                    </div>
+                                ))
+                            }
+                            {
+                                propsNoDestacadas?.map(p => (
+                                    <div className='cont-card' key={p.id}>
+                                        <Card
+                                            className='card'
+                                            id={p.id}
+                                            direccionF={p.direccionF}
+                                            operacion={p.operacion}
+                                            imagenes={p.imagenes}
+                                            tituloPublicacion={p.tituloPublicacion}
+                                            ambientes={p.ambientes}
+                                            dormitorios={p.dormitorios}
+                                            unidadMedida={p.unidadMedida}
+                                            cantCocheras={p.cantCocheras}
+                                            supTotal={p.supTotal}
+                                            tipo={p.tipo}
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </>
+                    ) : (
                         <div className='no-props'>
-                            <NoHayProps/>
+                            <NoHayProps />
                         </div>
                     )
                 }
